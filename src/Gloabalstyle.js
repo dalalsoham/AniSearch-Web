86e7a2610262113855a18561c59a4b7e64289c01
mr_sd_jack_003 <barson0habra@gmail.com>
2023-06-28 18:04:40 +0530

    update font style

--- src/Gloabalstyle.js
+++ src/Gloabalstyle.js
@@ -1,14 +1,14 @@
 import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Exo:wght@500&family=Kaushan+Script&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Exo:wght@500&family=Kaushan+Script&family=Playfair+Display&display=swap');
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         list-style: none;
         text-decoration: none;
-        font-family: 'Kaushan Script', cursive;
+        font-family: 'Playfair Display', serif;
     }
 
     body{
